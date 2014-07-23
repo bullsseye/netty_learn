af282a59a9a8b649a3738b45e7ea2103c7c31236
Raman <raman@hike.in>
2014-07-23 09:20:40 +0530

    First git echo server

---
+++ src/io/netty/example/discard/DiscardServerHandler.java
@@ -0,0 +1,34 @@
+package io.netty.example.discard;
+
+import io.netty.buffer.ByteBuf;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+/**
+ * Handles a server-side channel.
+ */
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter{ // (1)
+	
+	@Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+    	ByteBuf in = (ByteBuf) msg;
+        try {
+            while (in.isReadable()) { // (1)
+                System.out.print((char) in.readByte());
+                System.out.flush();
+            }
+        } finally {
+        	System.out.println("hey there whats up");
+          // in.release(msg); // (2)
+        }
+    	//System.out.println("Hey there whats up two");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
